2348c9f00c5abc4665d5b981dec10280807bf95c
Nikhil Tanaji More <nikhilmore54@rediffmail.com>
2016-06-03 23:24:40 +0530

    Implemets quickstart from laravel.com

---
+++ task3/myapp/database/migrations/2016_06_03_135522_create_users_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable1 extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('name')->default('');
+            $table->string('pass')->default('');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('users');
+    }
+}
