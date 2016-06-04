ac95187547d01b10c5b0361665640b0ce32f1e37
Nikhil Tanaji More <nikhilmore54@rediffmail.com>
2016-06-04 12:29:46 +0530

    Create view of register page

---
+++ task3/try1/resources/views/users.blade.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Laravel</title>
+
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+
+        <style>
+            html, body {
+                height: 100%;
+            }
+
+            body {
+                margin: 0;
+                padding: 0;
+                width: 100%;
+                display: table;
+                font-weight: 100;
+                font-family: 'Lato';
+            }
+
+            .container {
+                text-align: center;
+                display: table-cell;
+                vertical-align: middle;
+            }
+
+            .content {
+                text-align: center;
+                display: inline-block;
+            }
+
+            .title {
+                font-size: 96px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <form action="{{ url('user') }}" method="POST" class="form-horizontal">
+                    {{ csrf_field() }}
+
+                    <!-- User Name -->
+                    <div class="form-group">
+                        <label for="user">User</label>
+
+                        <input type="text" name="name" id="task-name">
+                    </div>
+
+                    <!-- Email Id -->
+                    <div class="form-group">
+                        <label for="email">E-mail id</label>
+
+                        <input type="text" name="email" id="email-id">
+                    </div>
+                   
+                    <!-- Add Register Button -->
+                    <button type="submit">
+                         Register
+                    </button>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
